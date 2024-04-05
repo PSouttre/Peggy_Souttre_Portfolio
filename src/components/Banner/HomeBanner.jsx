d161caacc9c2af5932d90aa843bfd02e93990981
PSouttre <peggy.souttre@free.fr>
2024-04-05 16:45:53 +0200

    banner

--- src/components/Banner/HomeBanner.jsx
+++ src/components/Banner/HomeBanner.jsx
@@ -3,16 +3,16 @@ import img from '../../assets/images/photo_PS.png'
 
 const HomeBanner = () => {
     return (
-    <div className='homebanner'>
-        <div className='img__banner'>
+    <div className='banner'>
+        <div className='banner__img'>
             <img src={img} alt="Peggy Souttre, développeuse web" />
         </div>
-        <div className='text__banner'>
-            <p>Après des années comme déléguée médicale en nutrition puis professeure en lycée professionnel, j'ai souhaité me reconvertir dans un milieu professionnel dynamique et où le partage est de rigueur.</p>
+        <div className='banner__text'>
+            <p className='banner__text__before'>Après des années comme déléguée médicale en nutrition puis professeure en lycée professionnel, j'ai souhaité me reconvertir dans un milieu professionnel <strong>dynamique</strong> et où le <strong>partage</strong> est de rigueur.</p>
             <br />
-            <p>Partie "from scratch", me voilà développeuse web !</p>
+            <p className='banner__text__now'>Partie "from scratch", me voilà développeuse web !</p>
             <br />
-            <p>Je recherche maintenant une alternance en Alsace pour continuer à évoluer dans ce domaine passionnant. </p>
+            <p className='banner__text__next'>Je recherche maintenant une <strong>alternance en Alsace</strong> pour continuer à évoluer dans ce domaine passionnant. </p>
         </div>
     </div>
     )
