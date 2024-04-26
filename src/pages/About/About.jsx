95f59e9ec28beb3c5a664e0181791a9e5cfc839d
PSouttre <peggy.souttre@free.fr>
2024-04-26 15:18:07 +0200

    projects page image position

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <HomeBanner/>
 
             <div className="container__collapse">
-                <Collapse title="Formation Developpeuse" content="Formation OpenClassRooms 2023/2024  Développeur Web. Diplôme de niveau 5 (bac + 2). Compétences acquises : construire un site web responsive et dynamique, créer des API et des bases de données, optimiser les performances et réaliser la maintenance de sites web déjà existants, gérer un projet web de A à Z.
+                <Collapse title="Formation Développeuse" content="Formation OpenClassRooms 2023/2024  Développeur Web. Diplôme de niveau 5 (bac + 2). Compétences acquises : construire un site web responsive et dynamique, créer des API et des bases de données, optimiser les performances et réaliser la maintenance de sites web déjà existants, gérer un projet web de A à Z.
                 Téchnologies et langages : HTML, CSS, JavaScript, React, Git/GitHub, NodeJS, Express, MongoDB, API REST" />
 
                 <Collapse title="Parcours professionnel" content="Mes années comme déléguée médicale puis professeure m'ont permise de développer mon relationnel aux autres, mes compétences de formatrice et enseignante et de faire preuve de beaucoup (beaucoup) d'autonomie." />
